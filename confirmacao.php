fc4d91aa57d26b7637629c414dabc124795d8208
LumiereSolucoes <111324863+LumiereSolucoes@users.noreply.github.com>
2022-12-04 23:20:24 -0300

    Add files via upload

---
+++ confirmacao.php
@@ -0,0 +1,24 @@
+<?php
+
+session_start();
+$cdg=$_SESSION['cdg'];
+$email           =  $_SESSION['email'];
+$senha           =  $_SESSION['senha'];
+$nome            =  $_SESSION['nome'];
+$telefone        =  $_SESSION['telefone'];
+
+ if ($_POST['codigo']==$cdg) { 
+
+    include_once './class/Cadastro.class.php';
+    $usuario = new Usuario( $email, md5($senha), $nome, $telefone);
+    $usuario->insert();
+    echo "<script>alert('Cadastro feito com sucesso!');location.href=\"login.html\";</script>";
+    
+ } else {
+
+    echo "<script>alert('Código incorreto!');location.href=\"cadastro.html\";</script>";
+
+ }
+
+
+    ?>
